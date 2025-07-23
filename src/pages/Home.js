da61c46de282a47f05990f76cfdbfbeb09427dfb
bmp0404 <KRISHDHAN@ICLOUD.COM>
2025-07-23 17:04:28 -0500

    added ability to order smoothies by diff factors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,17 +7,20 @@ import SmoothieCard from "../components/SmoothieCard"
 const Home = () => {
   const [fetchError, setFetchError] = useState(null)
   const [smoothies, setSmoothies] = useState(null)
+  const [orderBy, setOrderBy] = useState('created_at')
 
   const handleDelete = (id) => {
     setSmoothies(prevSmoothies => {
       return prevSmoothies.filter(sm => sm.id !== id)
     })
   }
+
   useEffect(() => {
     const fetchSmoothies = async () => {
       const { data, error } = await supabase
         .from('smoothies')
         .select()
+        .order(orderBy, { ascending: false })
 
       if (error) {
         setFetchError('Could not fetch the smoothies')
@@ -32,7 +35,7 @@ const Home = () => {
 
     fetchSmoothies()
 
-  }, [])
+  }, [orderBy])
 
 
   return (
@@ -40,7 +43,13 @@ const Home = () => {
       {fetchError && (<p>{fetchError}</p>)}
       {smoothies && (
         <div className="smoothies">
-          {/* order-by buttons */}
+          <div className="order-by">
+            <p>Ordery by:</p>
+            <button on onClick={() => setOrderBy('created_at')}>Date</button>
+            <button onClick={() => setOrderBy('rating')}>Rating</button>
+            <button onClick={() => setOrderBy('title')}>Title</button>
+            {orderBy}
+          </div>
           <div className="smoothie-grid">
             {smoothies.map(smoothie => (
               <SmoothieCard key={smoothie.id} smoothie={smoothie} onDelete={handleDelete} />
